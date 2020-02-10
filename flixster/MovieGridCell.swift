7d65999111ebea1fdcf2f97cab9be9f5c593c47a
Howard Aguilar <haguilar@csumb.edu>
2020-02-09 19:37:41 -0800

    Flix Part 2 Complete

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,18 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Howard Aguilar on 2/9/20.
+//  Copyright © 2020 Howard Aguilar. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+    
+}
